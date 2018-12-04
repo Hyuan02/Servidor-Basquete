8755d86629bfde1c59ced768b5203d971b1232f0
Junior <martoniojunior@hotmail.com>
2018-12-04 16:42:43 -0300

    ordenacao de ranking feita

--- teste.js
+++ teste.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
   }
   else{
     let consultaBasquete = db.get('pontuacaoRegistrada');
-    consultaBasquete.find({},{sort:{id:-1}}, function(err,data){
+    consultaBasquete.find({},{sort:{_pontuacao:-1}}, function(err,data){
       if(!err){
         //console.log(data);
         res.render('teste_html',{listaBasquete:data});
@@ -32,9 +32,9 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/pontuacaoTemporaria', function(req,res){
+app.post('/pontuacaoTemporaria', function(req,res){
     let basquete = db.get('pontuacaoTemporaria');
-    basquete.insert({_pontuacao: Math.floor(Math.random() * 10)}).then(function(){
+    basquete.insert({_pontuacao: req.body.points}).then(function(){
       console.log("Registrado!");
       registroPonto = true;
       res.redirect('/');      
@@ -51,12 +51,14 @@ app.post('/pontuacaoBasquete', function(req,res){
     let basquete = db.get('pontuacaoRegistrada');
     let pontoTemporario = db.get('pontuacaoTemporaria');
     pontoTemporario.findOne({}, {sort:{id:1},}, function(err, data){
-      if(!err)
-      basquete.insert({_nome: nomePontuador, _pontuacao: data._pontuacao}).then(function(){
+      if(!err){
+      	let pontuacao = parseInt(data._pontuacao);
+      	basquete.insert({_nome: nomePontuador, _pontuacao: pontuacao}).then(function(){
         pontoTemporario.drop();
         registroPonto = false;
-        res.redirect('/');
-      });
+       	res.redirect('/');
+      	});
+      }
     });
 });
 
